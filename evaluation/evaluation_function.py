63801d0b28c42d68eca0a4fb67eb27e4a726c3af
ikevin98 <ikevin98@naver.com>
2022-04-19 13:01:57 +0900

    doyoung code added

---
+++ evaluation/evaluation_function.py
@@ -0,0 +1,157 @@
+
+import argparse
+import logging
+import os
+import random
+import json
+
+import datasets
+import torch
+from datasets import load_dataset, load_metric
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+import transformers
+from accelerate import Accelerator
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    default_data_collator,
+)
+
+from t0.data_collator import DataCollatorForMultipleChoice
+from t0.model import ModelBase
+
+
+def evaluation(accelerator, tokenizer, model, args, eval_dataset, logger, task_name):
+    # Log a few random samples from the eval set:
+    prediction_gather = []
+    target_gather = []
+    for index in random.sample(range(len(eval_dataset)), 3):
+        logger.info(f"Sample {index} of the training set: {eval_dataset[index]}.")
+
+    # DataLoaders creation:
+    if args.pad_to_max_length:
+        # If padding was already done ot max length, we use the default data collator that will just convert everything
+        # to tensors.
+        data_collator = default_data_collator
+    else:
+        # Otherwise, `DataCollatorWithPadding` will apply dynamic padding for us (by padding to the maximum length of
+        # the samples passed). When using mixed precision, we add `pad_to_multiple_of=8` to pad all tensors to multiple
+        # of 8s, which will enable the use of Tensor Cores on NVIDIA hardware with compute capability >= 7.5 (Volta).
+        data_collator = DataCollatorForMultipleChoice(
+            tokenizer, pad_to_multiple_of=(8 if accelerator.use_fp16 else None)
+        )
+
+    eval_dataloader = DataLoader(eval_dataset, collate_fn=data_collator, batch_size=args.per_device_eval_batch_size)
+    # Use the device given by the `accelerator` object.
+    if not args.parallelize:
+        model.to(accelerator.device)
+
+    # Prepare everything with our `accelerator`.
+    eval_dataloader = accelerator.prepare(eval_dataloader)
+
+
+    # Metrics
+    metric = load_metric("accuracy")
+
+    # Eval!
+    total_batch_size = args.per_device_eval_batch_size * accelerator.num_processes
+
+    logger.info("***** Running evaluation *****")
+    logger.info(f"  Num examples = {len(eval_dataset)}")
+    logger.info(f"  Instantaneous batch size per device = {args.per_device_eval_batch_size}")
+    logger.info(f"  Total eval batch size (w. parallel, distributed) = {total_batch_size}")
+    # Only show the progress bar once on each machine.
+    progress_bar = tqdm(range(len(eval_dataloader)), disable=not accelerator.is_local_main_process)
+
+    model.eval()
+    for batch in eval_dataloader:
+        with torch.no_grad():
+            predictions = model(batch)
+            prediction_gather.append(predictions)
+            target_gather.append(batch["targets"])
+
+        metric.add_batch(
+            predictions=accelerator.gather(predictions),
+            references=accelerator.gather(batch["targets"]),
+        )
+
+        progress_bar.update(1)
+
+    eval_metric = metric.compute()
+    accelerator.print(f"Result: {eval_metric}")
+
+    results = {
+        "dataset_name": args.dataset_name,
+        "dataset_config_name": args.dataset_config_name,
+        "template_name": args.template_name,
+        "task_name" : task_name,
+        "evaluation": eval_metric
+    }
+    if accelerator.is_main_process:
+        if args.output_dir is not None:
+            with open(os.path.join(args.output_dir, f"{task_name}.json"), "w") as f:
+                json.dump(results, f, indent=4)
+    return prediction_gather, target_gather
+
+def evaluation_agreement(accelerator, tokenizer, model, args, eval_dataset_or_list, target_dataset_or_list, logger, task_name):
+    if type(eval_dataset_or_list)!= list:
+        eval_list = evaluation(accelerator, tokenizer, model, args, eval_dataset_or_list, logger, task_name)
+    else:
+        eval_list = eval_dataset_or_list
+    if type(target_dataset_or_list)!= list:
+        target_list = evaluation(accelerator, tokenizer, model, args, target_dataset_or_list, logger, task_name) 
+    else:
+        target_list = target_dataset_or_list   
+
+    # DataLoaders creation:
+    if args.pad_to_max_length:
+        # If padding was already done ot max length, we use the default data collator that will just convert everything
+        # to tensors.
+        data_collator = default_data_collator
+    else:
+        # Otherwise, `DataCollatorWithPadding` will apply dynamic padding for us (by padding to the maximum length of
+        # the samples passed). When using mixed precision, we add `pad_to_multiple_of=8` to pad all tensors to multiple
+        # of 8s, which will enable the use of Tensor Cores on NVIDIA hardware with compute capability >= 7.5 (Volta).
+        data_collator = DataCollatorForMultipleChoice(
+            tokenizer, pad_to_multiple_of=(8 if accelerator.use_fp16 else None)
+        )
+
+    # Metrics
+    metric = load_metric("accuracy")
+
+    # Eval!
+    total_batch_size = args.per_device_eval_batch_size * accelerator.num_processes
+
+    logger.info("***** Running evaluation agreement *****")
+    logger.info(f"  Num examples = {len(eval_list)}")
+    logger.info(f"  Instantaneous batch size per device = {args.per_device_eval_batch_size}")
+    logger.info(f"  Total eval batch size (w. parallel, distributed) = {total_batch_size}")
+    # Only show the progress bar once on each machine.
+    progress_bar = tqdm(range(len(eval_list)), disable=not accelerator.is_local_main_process)
+
+    model.eval()
+    for i in range(len(eval_list)):
+
+        metric.add_batch(
+            predictions=accelerator.gather(eval_list[i]),
+            references=accelerator.gather(target_list[i]),
+        )
+
+        progress_bar.update(1)
+
+    eval_metric = metric.compute()
+    accelerator.print(f"Result: {eval_metric}")
+
+    results = {
+        "dataset_name": args.dataset_name,
+        "dataset_config_name": args.dataset_config_name,
+        "template_name": args.template_name,
+        "task_name": task_name,
+        "evaluation": eval_metric
+    }
+    if accelerator.is_main_process:
+        if args.output_dir is not None:
+            with open(os.path.join(args.output_dir, f"{task_name}.json"), "w") as f:
+                json.dump(results, f, indent=4)
---
+++ evaluation/run_eval_instructive_negation_null.py
@@ -0,0 +1,343 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright BigScience, The HuggingFace Team and The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Reproduce the main evaluation in `Multitask Prompted Training Enables Zero-Shot Task Generalization` using PyTorch.
+
+This script is heavily adapted from https://github.com/huggingface/transformers/blob/7533d30acd975027e83a548e4c38e06fa335291b/examples/pytorch/multiple-choice/run_swag_no_trainer.py
+"""
+
+import argparse
+import logging
+import os
+import random
+import json
+import pandas as pd
+import datasets
+import torch
+from datasets import load_dataset, load_metric
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+import transformers
+from accelerate import Accelerator
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    default_data_collator,
+)
+from promptsource.templates import DatasetTemplates
+
+from t0.data_collator import DataCollatorForMultipleChoice
+from t0.model import ModelBase
+
+from evaluation_function import evaluation, evaluation_agreement
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Reproduce main evaluation in T0.")
+    parser.add_argument(
+        "--dataset_name",
+        type=str,
+        default=None,
+        help="The name of the dataset to use (via the datasets library).",
+        required=True,
+    )
+    parser.add_argument(
+        "--dataset_config_name",
+        type=str,
+        default=None,
+        help="The configuration name of the dataset to use (via the datasets library).",
+    )
+    parser.add_argument(
+        "--template_name",
+        type=str,
+        default=None,
+        help="The template/prompt name",
+        required=True,
+    )
+    parser.add_argument(
+        "--max_length",
+        type=int,
+        default=1024,
+        help=(
+            "The maximum total input sequence length after tokenization. Sequences longer than this will be truncated,"
+            " sequences shorter will be padded if `--pad_to_max_lengh` is passed."
+        ),
+    )
+    parser.add_argument(
+        "--target_max_length",
+        type=int,
+        default=256,
+        help="Target max length. Sequences longer than this will be truncated."
+    )
+    parser.add_argument(
+        "--pad_to_max_length",
+        action="store_true",
+        help="If passed, pad all samples to `max_length`. Otherwise, dynamic padding is used.",
+    )
+    parser.add_argument(
+        "--model_name_or_path",
+        type=str,
+        help="Path to pretrained model or model identifier from huggingface.co/models. The list of T0 variants can be found on `https://huggingface.co/bigscience/T0_3B`",
+        required=True,
+    )
+    parser.add_argument(
+        "--config_name",
+        type=str,
+        default=None,
+        help="Pretrained config name or path if not the same as model_name",
+    )
+    parser.add_argument(
+        "--tokenizer_name",
+        type=str,
+        default=None,
+        help="Pretrained tokenizer name or path if not the same as model_name",
+    )
+    parser.add_argument(
+        "--use_slow_tokenizer",
+        action="store_true",
+        help="If passed, will use a slow tokenizer (not backed by the 🤗 Tokenizers library).",
+    )
+    parser.add_argument(
+        "--per_device_eval_batch_size",
+        type=int,
+        default=8,
+        help="Batch size (per device) for the evaluation dataloader.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default=None,
+        help="Where to store the final model."
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Activate debug mode and run training only with a subset of data.",
+    )
+    parser.add_argument(
+        "--parallelize",
+        action="store_true",
+        help=(
+            "If passed, will call `model.parallelize` which splits the model on all GPUs available when applicable (model parallelism). "
+            "Note that this feature is still experimental in HF Transformers."
+        ),
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+
+
+def main():
+    args = parse_args()
+
+    # Initialize the accelerator. We will let the accelerator handle device placement for us.
+    accelerator = Accelerator()
+    # Make one log on every process with the configuration for debugging.
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    logger.info(accelerator.state)
+
+    # Setup logging, we only want one process per machine to log things on the screen.
+    # accelerator.is_local_main_process is only True for one process per machine.
+    logger.setLevel(logging.INFO if accelerator.is_local_main_process else logging.ERROR)
+    if accelerator.is_local_main_process:
+        datasets.utils.logging.set_verbosity_warning()
+        transformers.utils.logging.set_verbosity_info()
+    else:
+        datasets.utils.logging.set_verbosity_error()
+        transformers.utils.logging.set_verbosity_error()
+
+
+    # Handle the output directory creation
+    if accelerator.is_main_process:
+        os.makedirs(args.output_dir, exist_ok=True)
+    accelerator.wait_for_everyone()
+
+    # In distributed evaluation, the load_dataset function guarantee that only one local process can concurrently
+    # download the dataset.
+    if args.dataset_name is not None:
+        # Downloading and loading a dataset from the hub.
+        if args.dataset_name in ['imdb', 'ag_news', 'amazon_polarity', 'yelp_review_full' , 'dbpedia_14', 'trec', 'dream']:
+            raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name, split="test")        
+        elif args.dataset_name == 'wiki_bio':
+            raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name, split="val")
+        elif args.dataset_name == 'story_cloze':
+            raw_datasets = load_dataset('csv', data_files="datasets/cloze_test_val__spring2016 - cloze_test_ALL_val.csv")['train']
+        elif args.dataset_name == "anli":
+            raw_datasets = load_dataset(args.dataset_name, split=args.dataset_config_name)
+        else:
+            raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name, split="validation")
+        if len(raw_datasets)>500:
+            shuffled = raw_datasets.shuffle(seed=40)
+            raw_datasets=shuffled.select(range(500))
+    #TODO(Victor): enable loading pre-processed dataset from https://huggingface.co/datasets/bigscience/P3
+
+    # Trim a number of evaluation examples
+    if args.debug:
+        raw_datasets = raw_datasets.select(range(100))
+
+    column_names = raw_datasets.column_names
+
+
+    # Load pretrained model and tokenizer
+    #
+    # In distributed training, the .from_pretrained methods guarantee that only one local process can concurrently
+    # download model & vocab.
+    if args.config_name:
+        config = AutoConfig.from_pretrained(args.config_name)
+    elif args.model_name_or_path:
+        config = AutoConfig.from_pretrained(args.model_name_or_path)
+    else:
+        raise ValueError(
+            "Either `args.config_name` or `args.model_name_or_path` should be provided."
+        )
+
+    if args.tokenizer_name:
+        tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name, use_fast=not args.use_slow_tokenizer)
+    elif args.model_name_or_path:
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, use_fast=not args.use_slow_tokenizer)
+    else:
+        raise ValueError(
+            "You are instantiating a new tokenizer from scratch. This is not supported by this script."
+            "You can do it from another script, save it, and load it from here, using --tokenizer_name."
+        )
+
+    if tokenizer.pad_token is None:
+        for token in [tokenizer.eos_token, tokenizer.bos_token, tokenizer.sep_token]:
+            if token is not None:
+                tokenizer.pad_token = token
+        if tokenizer.pad_token is None:
+            raise ValueError("Please define a pad token id.")
+
+
+    model = ModelBase.from_config(
+        config=config,
+        model_name_or_path=args.model_name_or_path,
+        parallelize=args.parallelize
+    )
+
+    # Preprocessing the datasets.
+    # First we tokenize all the texts.
+    padding = "max_length" if args.pad_to_max_length else False
+
+    # Get the prompt to apply and the possible targets.
+    # TODO(Victor): If pulling from pre-processed data, remove this logic.
+    prompts = DatasetTemplates(
+        f"{args.dataset_name}"
+        if args.dataset_config_name is None
+        else f"{args.dataset_name}/{args.dataset_config_name}"
+    )
+    template = prompts[args.template_name]
+    template_negation = prompts[args.template_name + " negation"]
+    template_null = prompts[args.template_name + " null"]
+
+    def preprocess_function(examples, template):
+        bs = len(examples[column_names[0]])
+
+        input_texts = []
+        target_texts = []
+        answer_choices_texts = []
+        for i in range(bs):
+            ex = {
+                k: examples[k][i]
+                for k in column_names
+            }
+            input, target = template.apply(ex)
+            
+            ex_answer_choices = template.get_answer_choices_list(ex)
+            #print(target)
+            assert target in ex_answer_choices
+            input_texts.append(input)
+            target_texts.append(target)
+            answer_choices_texts.append(ex_answer_choices)
+
+        tokenized_inputs = tokenizer(
+            input_texts,
+            padding=padding,
+            max_length=args.max_length,
+            truncation=True,
+            add_special_tokens=False,
+        )
+        tokenized_targets = [
+            tokenizer(
+                ans_choi,
+                padding=True,
+                max_length=args.target_max_length,
+                truncation=True,
+            )
+            for ans_choi in answer_choices_texts
+        ]
+
+        features = {
+            k: [
+                [elem for _ in range(len(tokenized_targets[idx]["input_ids"]))]
+                for idx, elem in enumerate(v)
+            ]
+            for k, v in tokenized_inputs.items()
+        }
+
+        features["labels"] = [
+            tokenized_targets[idx]["input_ids"]
+            for idx in range(bs)
+        ]
+        features["labels_attention_mask"] = [
+            tokenized_targets[idx]["attention_mask"]
+            for idx in range(bs)
+        ]
+        features["targets"] = [
+            answer_choices_texts[idx].index(t)
+            for idx, t in enumerate(target_texts)
+        ]
+
+        return features
+
+    with accelerator.main_process_first():
+        def preprocess_function_instructive(example):
+            return preprocess_function(example, template=template)
+        def preprocess_function_negation(example):
+            return preprocess_function(example, template=template_negation)
+        def preprocess_function_null(example):
+            return preprocess_function(example, template=template_null)
+        eval_dataset = raw_datasets.map(
+            preprocess_function_instructive, batched=True, remove_columns=column_names
+        )
+        eval_dataset_negation = raw_datasets.map(
+            preprocess_function_negation, batched=True, remove_columns=column_names
+        )
+        eval_dataset_null = raw_datasets.map(
+            preprocess_function_null, batched=True, remove_columns=column_names
+        )
+
+
+    eval_list, target_gather = evaluation(accelerator, tokenizer, model, args, eval_dataset, logger, "instructive")
+    eval_list_negation, target_gather_negation = evaluation(accelerator, tokenizer, model, args, eval_dataset_negation, logger, "negation")
+    print(target_gather)
+    print(target_gather_negation)
+    #evaluation_agreement(accelerator, tokenizer, model, args, eval_list_negation,eval_list, logger, "agreement_instructive_negation")
+    #evaluation_agreement(accelerator, tokenizer, model, args, eval_dataset_null,eval_list, logger, "agreement_instructive_null")
+
+
+
+if __name__ == "__main__":
+    main()
